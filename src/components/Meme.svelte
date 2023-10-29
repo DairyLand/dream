d2c0b0045c46153f9003f6a7e69925eee3ca4fed
DairyLand <farmerhunter@proton.me>
2023-10-29 14:24:53 -0400

    svelte compoentn

---
+++ src/components/Meme.svelte
@@ -0,0 +1,27 @@
+<script>
+  let meme = null;
+
+  async function fetchRandomMeme() {
+    try {
+      const response = await fetch('https://meme-api.com/gimme');
+      if (response.ok) {
+        const data = await response.json();
+        meme = data.url;
+      } else {
+        console.error('Failed to fetch a random meme.');
+      }
+    } catch (error) {
+      console.error('An error occurred while fetching a meme:', error);
+    }
+  }
+</script>
+
+<main>
+  <h1>Random Meme Generator</h1>
+
+  <button on:click={fetchRandomMeme}>Get Random Meme</button>
+
+  {#if meme}
+    <img src={meme} alt="Random Meme" style="max-width: 100%;" />
+  {/if}
+</main>
